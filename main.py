ca4406c60873491748777e41cae7cc0956100d95
norby4ever <petrleliaev@gmail.com>
2022-10-04 12:20:20 +0300

    Chat bot created

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from aiogram import Bot, Dispatcher, executor, types
+from wikipedia import search, summary, DisambiguationError
+
+API_TOKEN = '5696283636:AAGgyEf5XEXPX2oe2gPEfe5neGKobOhNdMM'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет!\nВведи название любимой команды и получишь ссылку на статью про неё в википедии)")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    answer = 'По вашему запросу ничего не нашлось. Попробуйте уточнить'
+    results = search(message.text, results=10, suggestion=False)
+    if len(results) == 0 or len(results[0]) == 0:
+        answer = 'Не удалось найти команду, попробуйте указать город'
+    for result in results:
+        try:
+            if 'football' in summary(result, auto_suggest=False)[:300]:
+                answer = 'en.wikipedia.org/wiki/' + result.replace(' ', '_')
+                break
+        except DisambiguationError:
+            answer = 'Так может называться не только команда. Пожалуйста, уточните запрос.'
+            break
+    await message.answer(answer)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
